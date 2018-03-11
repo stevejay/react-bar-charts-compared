b719b74ea53266a6bc892c4d075dce2fe2e07c9d
Steve Johns <steve@stevejay.net>
2018-03-11 00:37:17 +0000

    wip

--- src/actions/data.js
+++ src/actions/data.js
@@ -1,4 +1,5 @@
 import { createAction } from 'redux-actions'
+import _ from 'lodash'
 
 export const types = {
   BAR_CHART_DATA_UPDATED: 'BAR_CHART_DATA_UPDATED'
@@ -6,9 +7,18 @@ export const types = {
 
 export const barChartDataUpdated = createAction(types.BAR_CHART_DATA_UPDATED)
 
+const PEOPLE = ['Arthur', 'Bob', 'Charlie', 'Dave', 'Elsie', 'Fred', 'Grant']
+
 export function updateBarChartData () {
   return dispatch =>
     new Promise(resolve => {
-      dispatch(barChartDataUpdated({ data: [] }))
+      const people = _.take(_.shuffle(PEOPLE), 5).map(name => ({
+        key: name,
+        value: _.random(50) === 0 ? 0 : _.random(100)
+      }))
+
+      //   console.log('people', JSON.stringify(people))
+
+      dispatch(barChartDataUpdated({ data: people }))
     })
 }
