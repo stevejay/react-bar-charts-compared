ee725536880cdbd92b95957cd1e54911bae9d6e2
Steve Johns <steve@stevejay.net>
2018-03-31 10:40:43 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import './polyfills'
+import 'babel-polyfill'
 
 import { updateBarChartData } from './actions/data'
 import UpdateButtons from './components/update-buttons'
